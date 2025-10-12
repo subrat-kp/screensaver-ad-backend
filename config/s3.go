65d651ee1fe18ceac7bc0dda5e89fbf293a8fe05
Subrat Pattanaik <subrat.kp@samsung.com>
2025-10-12 12:36:21 +0530

    Add config/s3.go with S3 client configuration

---
+++ config/s3.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+var S3Client *s3.S3
+
+// InitS3 initializes the S3 client
+func InitS3() error {
+	region := os.Getenv("AWS_REGION")
+	accessKey := os.Getenv("AWS_ACCESS_KEY_ID")
+	secretKey := os.Getenv("AWS_SECRET_ACCESS_KEY")
+
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String(region),
+		Credentials: credentials.NewStaticCredentials(accessKey, secretKey, ""),
+	})
+	if err != nil {
+		return err
+	}
+
+	S3Client = s3.New(sess)
+	return nil
+}
+
+// GetS3Client returns the S3 client instance
+func GetS3Client() *s3.S3 {
+	return S3Client
+}
