2f6c777360d9bcad389705d2f3051e7fb3d44a30
Subrat Pattanaik <subrat.kp@samsung.com>
2025-10-13 12:26:27 +0530

    added sslmode to env

--- config/database.go
+++ config/database.go
@@ -18,14 +18,19 @@ func InitDatabase() error {
 	user := os.Getenv("DB_USER")
 	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE")
+
+	if sslmode == "" {
+		sslmode = "disable"
+	}
 
 	var dsn string
 	if password == "" {
-		dsn = fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable",
-			host, port, user, dbname)
+		dsn = fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=%s",
+			host, port, user, dbname, sslmode)
 	} else {
-		dsn = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-			host, port, user, password, dbname)
+		dsn = fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+			host, port, user, password, dbname, sslmode)
 	}
 
 	var err error
